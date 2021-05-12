178d04af6c14042fae52a286f940def99471ad83
finbyz <info@finbyz.com>
2021-05-12 13:54:50 +0530

    issue changes

--- finbyz/public/js/doctype_js/issue.js
+++ finbyz/public/js/doctype_js/issue.js
@@ -1,10 +1,13 @@
 
 frappe.ui.form.on('Issue', {
 	refresh : function(frm) {
-        
 		frm.add_custom_button(__("Go To Project Password"), function() {
             if (frm.doc.project){
-            return frappe.set_route("Form","Project Password",frm.doc.project); 
+                frappe.db.get_value("Project Password",{'project':frm.doc.project},'name',function(r){
+                    if(r.name){
+                        frappe.set_route("Form","Project Password",r.name)
+                    }
+                })      
         }
         else{
             frappe.msgprint("Enter Project");
