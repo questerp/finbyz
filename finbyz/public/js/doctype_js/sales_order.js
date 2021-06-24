1b7997d31b8ca7b5cd772db90c5cf2a8fe24829a
finbyz <info@finbyz.com>
2021-06-24 05:44:11 +0000

    changes

--- finbyz/public/js/doctype_js/sales_order.js
+++ finbyz/public/js/doctype_js/sales_order.js
@@ -1,3 +1,15 @@
+
+//Contact Filter
+cur_frm.fields_dict.other_contacts.grid.get_field("contact").get_query = function(doc) {
+	if(cur_frm.doc.customer) {
+		return {
+			query: "frappe.contacts.doctype.contact.contact.contact_query",
+			filters: { link_doctype: "Customer", link_name: cur_frm.doc.customer} 
+		};
+	}
+	else frappe.throw(__("Please set Customer"));
+};
+
 frappe.ui.form.on('Other Contact', {
 
 })
