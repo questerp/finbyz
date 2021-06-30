bd56013a5d80b91c08a1fd20d0fe3d958ecd6e97
finbyz <info@finbyz.com>
2021-06-30 12:56:38 +0000

    sales order changes

--- finbyz/api.py
+++ finbyz/api.py
@@ -649,7 +649,7 @@ def send_sales_order_mails():
 			
 		message = header(customer) + '' + table + '' + footer(actual_amount, outstanding)
 		#recipient  = ['nirali.satapara@finbyz.tech']
-		recipients = set(recipients)
+		recipients = list(set(recipients))
 		try:
 			make(recipients=recipients,
 				sender = sender,
